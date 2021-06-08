a857740376f6d50b5f221e7db4f6f6c532c3ccf1
Marcelo Sercheli <msercheli@gmail.com>
2021-06-08 10:26:14 -0400

    Add rez files. Issues with mayaUsdUI build and qt5 MOC. Build is hanging forever.

--- cmake/modules/FindMaya.cmake
+++ cmake/modules/FindMaya.cmake
@@ -157,11 +157,13 @@ endif()
 find_path(MAYA_INCLUDE_DIR
         maya/MFn.h
     HINTS
+        "$ENV{REZ_MAYA_ROOT}"
         "${MAYA_LOCATION}"
         "$ENV{MAYA_LOCATION}"
         "${MAYA_BASE_DIR}"
     PATH_SUFFIXES
         ../../devkit/include/
+        devkitBase/include/
         include/
     DOC
         "Maya's headers path"
@@ -170,11 +172,13 @@ find_path(MAYA_INCLUDE_DIR
 find_path(MAYA_LIBRARY_DIR
         OpenMaya
     HINTS
+        "$ENV{REZ_MAYA_ROOT}"
         "${MAYA_LOCATION}"
         "$ENV{MAYA_LOCATION}"
         "${MAYA_BASE_DIR}"
     PATH_SUFFIXES
         ../../devkit/include/
+        devkitBase/include/
         include/
     DOC
         "Maya's libraries path"
@@ -185,11 +189,13 @@ list(APPEND MAYA_INCLUDE_DIRS ${MAYA_INCLUDE_DIR})
 find_path(MAYA_DEVKIT_INC_DIR
        GL/glext.h
     HINTS
+        "$ENV{REZ_MAYA_ROOT}"
         "${MAYA_LOCATION}"
         "$ENV{MAYA_LOCATION}"
         "${MAYA_BASE_DIR}"
     PATH_SUFFIXES
         ../../devkit/plug-ins/
+        devkitBase/devkit/plug-ins/
     DOC
         "Maya's devkit headers path"
 )
