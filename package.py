4800bf4eef0d9018577e2bed677cede6229dda7b
Marcelo Sercheli <msercheli@gmail.com>
2021-08-31 09:40:30 -0400

    Remove comment

--- package.py
+++ package.py
@@ -43,7 +43,7 @@ private_build_requires = [
 
 variants = [
     ["platform-linux", "arch-x86_64", "os-centos-7", "maya-2022.0.sse.2", "python-2", "usd-20.08.sse.1", "!ptex"],
-    ["platform-linux", "arch-x86_64", "os-centos-7", "maya-2022.0.sse.3", "python-3", "usd-20.08.sse.1", "!ptex"],  # It is not finding Boost::python (don't know why)
+    ["platform-linux", "arch-x86_64", "os-centos-7", "maya-2022.0.sse.3", "python-3", "usd-20.08.sse.1", "!ptex"],
     ["platform-linux", "arch-x86_64", "os-centos-7", "maya-2022.0.sse.2", "python-2", "usd-21.05.sse.1", "!ptex"],
     ["platform-linux", "arch-x86_64", "os-centos-7", "maya-2022.0.sse.3", "python-3", "usd-21.05.sse.1", "!ptex"],
 ]
