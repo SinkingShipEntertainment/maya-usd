0d6ea18e9a0da00eba1b664f7f3cb5c7c34827fd
Neil Rubie <neilrubie@sinkingship.ca>
2025-08-11 21:20:10 -0400

    Added maya2025.3 version

--- package.py
+++ package.py
@@ -28,6 +28,7 @@ variants = [
     ["maya-2024.2", "python-3.9", "usd-23.11"],  # maya 2024 uses python3-10
     ["maya-2024.2", "python-3.10", "usd-23.11"],
     ["maya-2025.1", "python-3.11", "usd-23.11"],
+    ["maya-2025.3", "python-3.11", "usd-23.11"],
 ]
 
 # If want to use Ninja, run:
